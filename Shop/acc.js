ede6e3a785976c652dd18f17f51f24a373fbbb38
urvashi2004 <uy2204@gmail.com>
2024-03-31 18:05:06 +0530

    Shop cpmponents added

---
+++ Shop/acc.js
@@ -0,0 +1,9 @@
+// document.getElementByClass("back").addEventListener ("click", barterhome,false);
+
+// function barterhome() {
+//   window.location.assign("frontpage.html");
+// }
+
+function barterhome(){
+    window.location.assign("frontpage.html");
+ }
--- acc.js
+++
@@ -1,9 +0,0 @@
-// document.getElementByClass("back").addEventListener ("click", barterhome,false);
-
-// function barterhome() {
-//   window.location.assign("frontpage.html");
-// }
-
-function barterhome(){
-    window.location.assign("frontpage.html");
- }
